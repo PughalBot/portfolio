6e3e846c9c54b01df5c72df334b33af76359ad9e
PughalBot <77720496+PughalBot@users.noreply.github.com>
2023-06-29 04:37:33 +0530

    init

---
+++ src/components/Navbar/navData.js
@@ -0,0 +1,27 @@
+export const navLinks = [
+  {
+    label: 'Projects',
+    pathname: '/#project-1',
+  },
+  {
+    label: 'Profile',
+    pathname: '/#details',
+  },
+  {
+    label: 'Contact',
+    pathname: '/contact',
+  },
+];
+
+export const socialLinks = [
+  {
+    label: 'Linkedin',
+    url: 'https://www.linkedin.com/in/pughal/',
+    icon: 'twitter',
+  },
+  {
+    label: 'Github',
+    url: 'https://github.com/PughalBot',
+    icon: 'github',
+  },
+];
